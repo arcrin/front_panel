01c37c2bb13773403d87789596f6fa5ea7f20e20
Wen <wbai@deltacontrols.com>
2022-04-24 20:44:50 -0700

    kinetis config file added

--- main.cpp
+++ main.cpp
@@ -5,15 +5,6 @@ const int DataTable[] = {100, 101, 102, 103};
 
 static int Data01 = 1000;
 
-int linking(int argc, char **argv)
-{
-    static int Bss01;
-    int ret = 0;
-
-    printf("Bss=%d, Data=%d\n", Bss01++, Data01++);
-    Data01 = DataTable[Bss01%4];
-//    ret = (int)Num2Text[Bss01][0];
-//    const int localTab[] = {200, 201, 202, 203};
-//    Data01 = localTab[Bss01%4];
-
+int main() {
+    while(1);
 }
